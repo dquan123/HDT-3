00ad51adf3f23f15c5a57953381f3fa0f659ea77
Diego Quan <dquan@delicatessen502.com>
2025-02-08 15:48:27 -0600

    Insertion sort

--- Main.java
+++ Main.java
@@ -1,3 +1,19 @@
 public class Main {
-    //Prueba
+    public static void main(String[] args) {
+        ordenamientos o = new ordenamientos();
+        int[] vector = {5, 3, 8, 4, 2, 18, 24, 1, 2, 8, 3, 4, 7};
+
+
+        for (int num : vector) {
+            System.out.print(num + " ");
+        }
+
+        System.out.println("Quick");
+        int[] resultado = o.quick(vector);
+
+        for (int num : resultado) {
+            System.out.print(num + " ");
+        }
+
+    }
 }
